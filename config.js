0896c604378204a3a9758555e737983ad4cb3151
Jimmy Harrison <jimmy@punkave.com>
2016-01-21 15:10:32 -0500

    Add create-0.6 command to apostrophe-cli

--- config.js
+++ config.js
@@ -4,3 +4,4 @@ module.exports = config;
 
 config.SHELL_DEPENDS = ['git'];
 config.APOSTROPHE_BOILERPLATE = 'https://github.com/punkave/apostrophe-boilerplate.git';
+config.APOSTROPHE_BOILERPLATE_UNSTABLE = 'https://github.com/punkave/apostrophe-boilerplate.git --branch unstable --single-branch';
