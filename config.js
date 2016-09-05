750aac5608a07a47ad7f2cd5290a07d8eb94df60
Tom Boutell <tom@punkave.com>
2016-09-05 14:29:02 -0400

    Make sure we're defaulting properly to 2.x

--- config.js
+++ config.js
@@ -4,4 +4,4 @@ module.exports = config;
 
 config.SHELL_DEPENDS = ['git'];
 config.APOSTROPHE_BOILERPLATE = 'https://github.com/punkave/apostrophe-boilerplate.git';
-config.APOSTROPHE_BOILERPLATE_UNSTABLE = 'https://github.com/punkave/apostrophe-boilerplate.git --branch unstable --single-branch';
+config.APOSTROPHE_BOILERPLATE_OHFIVE = 'https://github.com/punkave/apostrophe-boilerplate.git --branch 0.5 --single-branch';
